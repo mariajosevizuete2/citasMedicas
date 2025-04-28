5d946c6d4b959179c5bd5336e46414ba8df105fd
mariajosevizuete2 <mjvizuete2@espe.edu.ec>
2025-04-27 22:36:14 -0500

    cambios en conf

--- app/config/config.php
+++ app/config/config.php
@@ -2,8 +2,8 @@
 
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'citas_medicas');
-define('DB_USER', 'majo');
-define('DB_PASS', 'majo123');
+define('DB_USER', 'root');
+define('DB_PASS', '');
 
 try {
     $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
